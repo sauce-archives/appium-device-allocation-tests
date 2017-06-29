916e88ca7141a0e306483a9b55ab71edcabaf19b
abdullahawara <abdulla@abdullas-MacBook-Pro.local>
2017-06-29 16:50:47 +0200

    Initial commit

---
+++ src/test/java/TestIOSAllocateByDeviceDescriptorId.java
@@ -0,0 +1,34 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.ios.IOSDriver;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class TestIOSAllocateByDeviceDescriptorId {
+
+	AppiumDriver driver;
+
+	@Before
+	public void setup() throws MalformedURLException {
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+
+		capabilities.setCapability("testobject_api_key", System.getenv("TESTOBJECT_API_KEY"));
+		capabilities.setCapability("testobject_device", System.getenv("DEVICE_DESCRIPTOR_ID"));
+
+		driver = new IOSDriver(new URL(System.getenv("APPIUM_SERVER")), capabilities);
+	}
+
+	@Test
+	public void printContext() throws Exception {
+		System.out.println(driver.getContext());
+	}
+
+	@After
+	public void tearDown() {
+		driver.quit();
+	}
+}
